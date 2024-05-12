d754a713610a12dfb49a7be016282703e73e0b4a
marym-ahmed <82773504+marym-ahmed@users.noreply.github.com>
2024-05-13 02:10:11 +0300

    First Commit

---
+++ realTimeNotification/storage/framework/views/c4a5ea9c598e7216f751d6e95ee60435.php
@@ -0,0 +1,90 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+                <button onclick="startFCM()"
+                    class="btn btn-danger btn-flat">Allow notification
+                </button>
+            <div class="card mt-3">
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo e(session('status')); ?>
+
+                    </div>
+                    <?php endif; ?>
+                    <form action="<?php echo e(route('send.web-notification')); ?>" method="POST">
+                        <?php echo csrf_field(); ?>
+                        <div class="form-group">
+                            <label>Message Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label>Message Body</label>
+                            <textarea class="form-control" name="body"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success btn-block">Send Notification</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase.js"></script>
+
+<script>
+    var firebaseConfig = {
+        apiKey: "AIzaSyDtRUeqwbOgPZp2cOQzUPegu2ZS7nahPc0",
+        authDomain: "mychatapp-f672a.firebaseapp.com",
+        projectId: "mychatapp-f672a",
+        storageBucket: "mychatapp-f672a.appspot.com",
+        messagingSenderId: "594261535823",
+        appId: "1:594261535823:web:ce4bb5adb2a83b375f4e3a",
+        measurementId: "G-784LKK2VTF"
+    };
+    firebase.initializeApp(firebaseConfig);
+    const messaging = firebase.messaging();
+    function startFCM() {
+        messaging
+            .requestPermission()
+            .then(function () {
+                return messaging.getToken()
+            })
+            .then(function (response) {
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                $.ajax({
+                    url: '<?php echo e(route("store.token")); ?>',
+                    type: 'POST',
+                    data: {
+                        token: response
+                    },
+                    dataType: 'JSON',
+                    success: function (response) {
+                        alert('Token stored.');
+                    },
+                    error: function (error) {
+                        alert(error);
+                    },
+                });
+            }).catch(function (error) {
+                alert(error);
+            });
+    }
+    messaging.onMessage(function (payload) {
+        const title = payload.notification.title;
+        const options = {
+            body: payload.notification.body,
+            icon: payload.notification.icon,
+        };
+        new Notification(title, options);
+    });
+</script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel10\resources\views/home.blade.php ENDPATH**/ ?>
