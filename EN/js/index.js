ab58b0948714cdc1cf73ab8752189979c5ac1ae3
Ibrahim94-Mohammed <74836246+Ibrahim94-Mohammed@users.noreply.github.com>
2024-05-15 02:06:14 +0200

    Translate

---
+++ EN/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#toggle-1").click(function(){
+        $("#team-data-1").toggle(); // Toggle the display of myElement
+    });
+});
+$(document).ready(function(){
+    $("#toggle-2").click(function(){
+        $("#team-data-2").toggle(); // Toggle the display of myElement
+    });
+});
+$(document).ready(function(){
+    $("#toggle-3").click(function(){
+        $("#team-data-3").toggle(); // Toggle the display of myElement
+    });
+});
+$(document).ready(function(){
+    $("#toggle-4").click(function(){
+        $("#team-data-4").toggle(); // Toggle the display of myElement
+    });
+});
