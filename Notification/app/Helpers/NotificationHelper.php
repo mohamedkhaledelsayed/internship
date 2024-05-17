c2e74a5057b37a850f3246c98d483b3cf2e57bd4
Homam Haidar <homamhaidar68@gmail.com>
2024-05-17 12:49:01 +0300

    make tool for send Notification
    add helper to send Notification

---
+++ Notification/app/Helpers/NotificationHelper.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Helpers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+function SendNotification(Request $request,$url,$FcmToken,$serverKey){
+
+
+
+    $data = [
+        "registration_ids" => $FcmToken,
+        "notification" => [
+            "title" => $request->title,
+            "body" => $request->body,
+        ]
+    ];
+    $encodedData = json_encode($data);
+
+    $headers = [
+        'Authorization:key=' . $serverKey,
+        'Content-Type: application/json',
+    ];
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    // Disabling SSL Certificate support temporarly
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+    // Execute post
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('Curl failed: ' . curl_error($ch));
+    }
+    // Close connection
+    curl_close($ch);
+    // FCM response
+    dd($result);
+}
--- Notification/app/Http/Controllers/NotificationSendController.php
+++ Notification/app/Http/Controllers/NotificationSendController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use function App\Helpers\Notificationsss;
+use function App\Helpers\SendNotification;
 
 class NotificationSendController extends Controller
 {
@@ -19,45 +22,11 @@ class NotificationSendController extends Controller
 
     public function sendNotification(Request $request)
     {
-
         $url = 'https://fcm.googleapis.com/fcm/send';
         $FcmToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+        $serverKey = 'AAAAqwiVsSc:APA91bFeInU9nhI8iljkWrM9dyMLypv7MXwwrZMg7UTNNgOYsRqwxsQxxf_nm3I_GaquGkwG60aYGxg_HsZdFl9L7IXxYxpNfaNFAWHrOAp1Jp_OgWSI4lSum3JiJ8-olo48jA_caHqJ';
 
-        $serverKey = 'AAAAqwiVsSc:APA91bFeInU9nhI8iljkWrM9dyMLypv7MXwwrZMg7UTNNgOYsRqwxsQxxf_nm3I_GaquGkwG60aYGxg_HsZdFl9L7IXxYxpNfaNFAWHrOAp1Jp_OgWSI4lSum3JiJ8-olo48jA_caHqJ'; // ADD SERVER KEY HERE PROVIDED BY FCM
+        SendNotification ($request,$url,$FcmToken,$serverKey);
 
-        $data = [
-            "registration_ids" => $FcmToken,
-            "notification" => [
-                "title" => $request->title,
-                "body" => $request->body,
-            ]
-        ];
-        $encodedData = json_encode($data);
-
-        $headers = [
-            'Authorization:key=' . $serverKey,
-            'Content-Type: application/json',
-        ];
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        // Disabling SSL Certificate support temporarly
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
-        // Execute post
-        $result = curl_exec($ch);
-        if ($result === FALSE) {
-            die('Curl failed: ' . curl_error($ch));
-        }
-        // Close connection
-        curl_close($ch);
-        // FCM response
-        dd($result);
     }
 }
